d4e48b67b0132c39bae9ea16c7d4377893ef5836
laurods <itrafor@gmail.com>
2021-02-06 04:10:10 -0200

    Iniciando projeto 060221a

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Grid from '@material-ui/core/Grid';
+function Index() {
+  return (
+    <div>
+     <CssBaseline />
+   
+
+      <main>
+        <Container>
+        <Grid container spacing={3}>
+        <Grid item xs={8}>       
+     
+        </Grid>
+        <Grid item xs={12}>       
+        <h1>Drink Eat</h1>
+        </Grid>
+        <Grid item xs={12}>       
+      
+        </Grid>
+
+
+
+        </Grid>
+        </Container>
+      </main>
+    </div>
+  );
+}
+
+export default Index;
