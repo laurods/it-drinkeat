f85cfd77b99be4c4c2397985fa930a7ab99e1458
laurods <itrafor@gmail.com>
2021-02-19 06:08:35 -0200

    login context_api 19022021b

--- contexts/authProvider.js
+++ contexts/authProvider.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import AuthContext from './authContext';
-import * as auth from "../pages/api/auth";
-import ApiAxios from "../pages/api/axios"
+import axios from 'axios';
+//import * as auth from "../pages/api/auth";
+//import ApiAxios from "../pages/api/axios"
 
 
 function Provider({ children }) {
@@ -23,26 +24,27 @@ function Provider({ children }) {
       loadStorageData();
     }, []);
 
-    async function signIn(values){
-        //const response = await auth.signIn(values);       
-        const response = await fetch('https://it-gestor.vercel.app/api/user/authDrinkeat', {
-            method:'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                //email: emailRef.current.value,
-                //password: passRef.current.value,
-                email: 'pedro@gmail.com',
-                password: '321',
-            })
-        });
+    async function signIn(){
+        //const response = await auth.signIn();       
+       
+        const user = {
+          email: 'pedro@gmail.com',
+          password: '321',
+        }
+        axios.post(`https://it-gestor.vercel.app/api/user/authDrinkeat`, { user }).then( response => {
+          const { _id, email } = response;
+        setUser(response.email);        
+        localStorage.setItem("@DrinkEat:user", JSON.stringify(response.email));
+        localStorage.setItem("@DrinkEat:token", JSON.stringify(response.id));
 
+        })
+        /*
         const { _id, email } = response;
         setUser(response.user);
         ApiAxios.defaults.headers['Authorization'] = `Bearer ${response._id}`;
         localStorage.setItem("@DrinkEat:user", JSON.stringify(response.email));
         localStorage.setItem("@DrinkEat:token", JSON.stringify(response.id));
+        */
       } 
 
     function signOut(){
